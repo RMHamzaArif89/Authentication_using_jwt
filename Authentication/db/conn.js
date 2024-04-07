fad86ca7c285ba3649caca25c3d2b469371e59e6
RMHamzaArif89 <ranaarifisonline@gmail.com>
2024-04-07 22:27:40 +0500

    created the connection for the db

--- Authentication/db/conn.js
+++ Authentication/db/conn.js
@@ -1,5 +1,6 @@
 mongoose=require('mongoose')
 
+
 mongoose.connect('mongodb://127.0.0.1:27017/jwt_authentication')
 .then(()=>console.log('connection is established'))
 .catch((err)=>console.log(err))
